4adec7328e04cc4d6f1d619159041b20142ea778
jose <tsukisan@gmail.com>
2022-03-09 12:55:23 +0200

    For resolutions

---
+++ basic_solution.rkt
@@ -0,0 +1,224 @@
+#lang racket
+;; importing for using.
+;; threading for piping operator.
+;; rackunit for testing.
+;; 09.03.2022 -- Finishing the searching
+(require threading rackunit dyoo-while-loop data/queue)
+
+;; Creamos structures para utilizar
+;; Cube  --> tiene una capacidad y un contenido.
+;; Cubes --> tiene una lista de cubos y un objetivo
+(struct Cube  (capacity content)  #:transparent)
+(struct Cubes (cubes        ;; list of Cube
+               goal         ;; objetive
+               )        #:transparent)
+
+;; Examples of cubos sencillos
+;; ---> con una capacidad y un contenido. 
+(define cube-0 (Cube 4 2))
+(define cube-1 (Cube 3 0))
+
+;; Examples de listas de cubos.
+(define lista-1 (Cubes (list (Cube 4 2)  (Cube 3 0)) 1))
+
+
+;; State cubes reflecting the state of the cubes
+;; From a state we get states 
+(struct State (cubes  ;; Cubes                   --> (Cubes (list (Cube 4 0)  (Cube 3 0)) 1))
+               states ;; Recursive form of state --> (State (Cubes (list (Cube 4 0) (Cube 3 0)) 1) 'None))
+               )      #:transparent)
+
+;; Passing from a State to a list of cubes
+;; State -> list of Cubes
+(define (state->list state [acc '()])
+  (cond
+    [(equal? state 'None) acc]  ;; Base case
+    [else
+     (let ([cubes (State-cubes state)]
+           [next  (State-states state)])
+       (state->list next (cons cubes acc)))]))
+  
+
+;; All posibilities of changing the state of the cubes
+;; Full all cubes
+;; Empty all cubes
+;; Transbase from one to another.
+
+;; Given two cubes make a transbase from one to another.
+;; Cube -> Cube -> (Pair Cube Cube)
+(define (transbase from to)
+  (let* ([cap1 (Cube-capacity from)]
+         [cap2 (Cube-capacity to)]
+         [con1 (Cube-content from)]
+         [con2 (Cube-content to)]
+         [receiving (min (- cap2 con2) con1)]
+         [new-con1 (- con1 receiving)]
+         [new-con2 (+ con2 receiving)])
+    (list (Cube cap1 new-con1) (Cube cap2 new-con2))))
+;; module test
+(module+  test
+  (check-equal?  (transbase (Cube 4 1) (Cube 3 0)) (list (Cube 4 0) (Cube 3 1)))
+  (check-equal?  (transbase (Cube 4 1) (Cube 3 2)) (list (Cube 4 0) (Cube 3 3)))
+  (check-equal?  (transbase (Cube 4 3) (Cube 3 2)) (list (Cube 4 2) (Cube 3 3)))
+  (check-equal?  (transbase (Cube 4 1) (Cube 3 1)) (list (Cube 4 0) (Cube 3 2)))
+  (check-equal?  (transbase (Cube 4 0) (Cube 3 2)) (list (Cube 4 0) (Cube 3 2)))
+  (check-equal?  (transbase (Cube 4 2) (Cube 3 0)) (list (Cube 4 0) (Cube 3 2)))
+  )
+;; empty
+;; Cube -> Cube
+(define (empting cube)
+   (let ([cap1 (Cube-capacity cube)])
+     (Cube cap1 0)))
+
+;; fullness
+;; Cube -> Cube
+(define (filling cube)
+  (let ([cap1 (Cube-capacity cube)])
+    (Cube cap1 cap1)))
+
+;; list of cubes -> function -> (listofcubes -> listofcubes)
+(define (apply-to-cubes function)
+  (define order  (λ (cube1 cube2)
+          (> (Cube-capacity cube1)
+             (Cube-capacity cube2))))
+  (λ (listofcubes)
+  (sort (cons (function (car listofcubes))
+              (rest listofcubes))
+        order)))
+       
+;; Applying transbase to all the 
+(define (apply-two-cubes function)
+  ;; auxiliar
+  (define order (λ (cube1 cube2)
+          (> (Cube-capacity cube1)
+             (Cube-capacity cube2))))
+  ;; function
+  (λ (listofcubes)
+    (let* ([from (car listofcubes)]
+           [r    (cdr listofcubes)]
+           [to   (car r)]
+           [rest (rest r)])
+    
+    (sort (append (function from to) rest) order))))
+
+
+;; state -> list of cubes
+(define (state->listofcubes state)
+    ;; auxiliar for applying the maps
+  (define (f-aux per f1 function)
+    (~>> per
+         (map (f1 function))))
+  ;; body function
+  (let* ([cubes (State-cubes state)]
+         [lofcubes (Cubes-cubes cubes)]
+         [g (Cubes-goal cubes)]
+         [per (permutations lofcubes)]
+         [from-fullfit  (f-aux per apply-to-cubes filling)]
+         [from-empting  (f-aux per apply-to-cubes empting)]
+         [from-trans    (f-aux per apply-two-cubes transbase)])
+ (~>>
+  (append from-empting  from-fullfit from-trans)
+  (map (λ (cubes) (Cubes cubes g))))))
+
+
+;; state -> setofcubes -> list of cubes filtered
+;; Only consider the Cubes that are not visited.
+(define (state->filtered set state)
+  (~>> state
+       state->listofcubes
+       (filter (λ (cube) (not (set-member? set cube))))))
+
+;; list of cubes genera all states from all combinations
+;; We only need to change one of them
+;; Cubes (list (Cube 4 0)  (Cube 3 0)) 1)
+(define (state->lofstates state)
+  (~>> state
+       state->listofcubes
+       (map (λ (loc) (State loc state)))))
+
+
+;; Now we need to create a bucle for looking for but considering
+;; we must avoid the Cubes we have visited.
+
+
+
+;; (state->lofstates (State lista-1 'None))
+
+;; (~>> (list 1 2 3)
+;;     permutations
+;;     (map (λ (l) (take l 2))))
+
+;; -----------------------------------------------------------------------------------------------------------------------
+;; ------------------------------------------------- searching part ------------------------------------------------------
+;; -----------------------------------------------------------------------------------------------------------------------
+;; Discover if a Cubes is a solution
+;; Cubes -> boolean
+(define (is-cube-solution? cubes)
+  (let ([goal (Cubes-goal cubes)])
+         (~>>
+          (Cubes-cubes cubes)
+          (map Cube-content)
+          (ormap (λ (x) (equal? x goal))))))
+
+(module+ test
+  ; true propositions
+  (check-true   (ormap (λ (x) (equal? x 2)) (list 1 2 3)))
+  (check-true   (is-cube-solution? (Cubes (list (Cube 4 0) (Cube 3 2)) 2)))
+  (check-true   (is-cube-solution? (Cubes (list (Cube 4 0) (Cube 3 2)) 0)))
+  (check-true   (is-cube-solution? (Cubes (list (Cube 4 4) (Cube 3 2)) 4)))
+  ; false propositions
+  (check-false  (is-cube-solution? (Cubes (list (Cube 4 0) (Cube 3 2)) 3)))
+  (check-false  (ormap (λ (x) (equal? x 4)) (list 1 2 3))))
+
+;; Discover if a State is a solution
+;; State -> boolean
+(define (is-state-solution? state)
+  (let ([cubes (State-cubes state)])
+    (is-cube-solution? cubes)))
+(module+ test
+  (define base      (State (Cubes (list (Cube 4 2) (Cube 3 0)) 1) 'None))
+  (define example-1 (State (Cubes (list (Cube 4 2) (Cube 3 0)) 1) base))
+  (define example-2 (State (Cubes (list (Cube 4 2) (Cube 3 0)) 2) base))
+  ;; true statements tests
+  (check-true  (is-state-solution? example-2))
+  (check-false (is-state-solution? example-1))
+  ;; false statements tests
+  (check-false (is-state-solution? base)))
+                          
+  
+;; searching from a initial state to find a solution
+;;
+(define (search initial-state)
+  (let* ([visited       (mutable-set)]     ;; record the visited states
+         [initial-cubes
+          (State-cubes initial-state)]     ;; initial cubes
+         [queue-states  (make-queue)]      ;; queue for the states
+         [solution 'None])                 ;; solution
+    (set-add! visited initial-cubes)       ;; visited            
+    (enqueue! queue-states initial-state)  ;; queue for the states
+    (while
+     (not (queue-empty? queue-states))     ;; while not empty the queue
+     ;; operating
+     (let*
+         ([state  (dequeue! queue-states)])
+       (cond
+         [(is-state-solution? state)       ;; we have a solution
+          (set! solution state)            ;; set the solution
+          (break)]                           ;; break
+         [else
+          (let* ([next-cubes (state->filtered visited state)]
+                 [next-states (~>>
+                               next-cubes
+                               (map (λ (cubes) (State cubes state))))])
+
+            (for-each (λ (cubes)  (set-add! visited cubes)) next-cubes)
+            (for-each (λ (state)  (enqueue! queue-states state)) next-states)
+            (continue))])))
+    solution))
+;; some simple tests
+(module+ test
+ (state->list (search base)))
+
+
+
+;; We just need a loop for the actualizing the visited until we reach the solution or we reach certain number of interations.
